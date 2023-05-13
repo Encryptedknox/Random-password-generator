76b62113e8ce9e0b4a1f03c5f1f179e92b30bb5c
knox <100846845+Encryptedknox@users.noreply.github.com>
2023-05-13 08:14:31 +0530

    Create index.js

---
+++ scr/index.js
@@ -0,0 +1,35 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// create Telegram bot object
+
+const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+
+const bot = new TelegramBot(token, { polling: true });
+
+// set up command listener for /password
+
+bot.onText(/\/password/, (msg) => {
+
+  
+
+  // generate random password
+
+  const length = 8; // password length
+
+  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; // password characters
+
+  let password = '';
+
+  for (let i = 0; i < length; i++) {
+
+    password += characters[Math.floor(Math.random() * characters.length)];
+
+  }
+
+  
+
+  // send the password to the user
+
+  bot.sendMessage(msg.chat.id, `Your random password is: ${password}`);
+
+});
